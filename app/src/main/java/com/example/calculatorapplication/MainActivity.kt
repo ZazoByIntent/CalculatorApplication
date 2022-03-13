a67a54259a9965cbe2d7d94f065fec9c1767e70b
Aljaž Hafner <87764692+ZazoByIntent@users.noreply.github.com>
2022-03-13 16:40:39 +0100

    Function implementation: calculate

--- app/src/main/java/com/example/calculatorapplication/MainActivity.kt
+++ app/src/main/java/com/example/calculatorapplication/MainActivity.kt
@@ -3,18 +3,46 @@ package com.example.calculatorapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.example.calculatorapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+
+    fun calculate(first: Int, second: Int, operation: String) : Int
+    {
+        if(operation == "add")
+        {
+            return first+second
+        }
+        else if (operation == "subtract")
+        {
+            return first-second
+        }
+        else {
+            return -1
+        }
     }
 
     fun onClickFirst(view: View?){
-
+        val first = binding.etFirstInput.text.toString().toInt()
+        val second = binding.etSecondInput.text.toString().toInt()
+        val operation = "add"
+        val result = calculate(first, second, operation)
+        binding.tvOutput.text = result.toString()
     }
 
     fun onClickSecond(view: View?){
-
+        val first = binding.etFirstInput.text.toString().toInt()
+        val second = binding.etSecondInput.text.toString().toInt()
+        val operation = "subtract"
+        val result = calculate(first, second, operation)
+        binding.tvOutput.text = result.toString()
     }
 }
