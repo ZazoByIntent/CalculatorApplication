74e99eaa65e49943987d0c13041b50b91ce66366
Aljaž Hafner <87764692+ZazoByIntent@users.noreply.github.com>
2022-03-13 16:17:40 +0100

    Initial commit

---
+++ app/src/main/java/com/example/calculatorapplication/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.calculatorapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun onClickFirst(view: View?){
+
+    }
+
+    fun onClickSecond(view: View?){
+
+    }
+}
